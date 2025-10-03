5850256a1ce2df7e36e64970cc1fdcaca2a82121
Aki Sakata <akisoqls@gmail.com>
2025-10-03 20:56:18 +0900

    Use .env file

--- src/RFPL-UI/commands/Call/index.ts
+++ src/RFPL-UI/commands/Call/index.ts
@@ -16,10 +16,8 @@ export class Call extends FetchCommand {
 
   public async exec(args: string[]): Promise<this> {
     this.args = args;
-    // const testRes = await fetch(args[0]);
-    // const htmlText = await testRes.text();
-
-    this.htmlElement.src = this.args[0];
+    const proxy = import.meta.env["VITE_PROXY"]
+    this.htmlElement.src = proxy + this.args[0];
     this.result.result = {
       contentType: "text/html",
       body: this.htmlElement,
